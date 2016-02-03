daf7d3e225ab9362914f08bbac21123e62f4a84f
cmuramoto <cmuramoto@nismo.nc.main>
2016-02-03 10:34:44 -0200

    Tests

--- hz-ser-bench/src/test/java/com/nc/gs/TestSSEDeserialization.java
+++ hz-ser-bench/src/test/java/com/nc/gs/TestSSEDeserialization.java
@@ -1,64 +1,71 @@
 package com.nc.gs;
 
 import java.io.BufferedReader;
+
+import static org.junit.Assert.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteOrder;
 
+import org.junit.Test;
+
 import com.nc.io.ByteArrayObjectDataInput;
 import com.nc.io.ByteArrayObjectDataOutput;
 
-public class TestSSE {
+public class TestSSEDeserialization {
 
 	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
+	@Test
+	public void sample() throws IOException {
 		ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder());
 
-		out.writeUTF("Poop");
-		out.writeUTF("Tart");
+		out.writeUTF("One");
+		out.writeUTF("Two");
 
 		ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.toByteArray(), null, ByteOrder.nativeOrder());
 
-		System.out.println(in.readUTFJNI());
-		System.out.println(in.readUTFJNI());
-
-		System.out.println("-----------------");
-		
-		danteLoop();
-		
-		System.out.println("-----------------");
-		
-		asciiLoop();
+		assertEquals("One", in.readUTFJNI());
+		assertEquals("Two", in.readUTFJNI());
 	}
 
-	public static void danteLoop() throws IOException {
+	@Test
+	public void danteLoop() throws IOException {
+
 		loop("paradiseXXXIII.txt");
 	}
 
-	public static void asciiLoop() throws IOException {
+	@Test
+	public void asciiLoop() throws IOException {
 		loop("pure_ascii.txt");
 	}
-	
+
 	private static void loop(String resource) throws IOException {
+		msg("-----------------");
 		String s = loadTextSource(resource);
 
-		System.out.println("Length " + s.length());
+		msg("Length %d\n", s.length());
 
 		regularLoop(s, 100, "Warm Up");
-		regularLoop(s, 100000, "Bench");
+		long[] rl = regularLoop(s, 100000, "Bench");
 
 		jniLoop(s, 100, "Warm Up");
-		jniLoop(s, 100000, "Bench");
+		long[] jl = jniLoop(s, 100000, "Bench");
+
+		msg("-----------------");
+
+		System.err.printf("\nString len: %d. Regular (elapsed): %d. JNI (elapsed): %d. Regular (elapsed/inflate-only): %d. JNI (elapsed/inflate-only): %d. \nRegular/JNI: %.2f.\nRegular/JNI (Inflate only): %.2f\n",s.length(), rl[0],jl[0],rl[1],jl[1], ((double) rl[0]) / jl[0],
+				((double) rl[1]) / jl[1]);
 	}
 
-	private static void jniLoop(String s, int itrs, String title) throws IOException {
+	private static long[] jniLoop(String s, int itrs, String title) throws IOException {
 		try (ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder())) {
 			out.writeUTF(s);
 			int len = s.length();
 			char[] dst = new char[len];
+			long[] rv = new long[2];
 			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null,
 					ByteOrder.nativeOrder())) {
-				System.out.println("Starting jni " + title);
+				msg("Starting jni %s\n", title);
 
 				long start = System.currentTimeMillis();
 				boolean ok = true;
@@ -73,8 +80,9 @@ public class TestSSE {
 				}
 
 				long elapsed = System.currentTimeMillis() - start;
+				rv[0] = elapsed;
 
-				System.out.printf("JNI[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
+				msg("JNI[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
 
 				if (!ok) {
 					System.err.println("Failed");
@@ -91,20 +99,24 @@ public class TestSSE {
 					in.reset();
 				}
 				elapsed = System.currentTimeMillis() - start;
+				rv[1] = elapsed;
 
-				System.out.printf("JNI(Inflate)[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
+				msg("JNI(Inflate)[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
+
+				return rv;
 			}
 		}
 	}
 
-	private static void regularLoop(String s, int itrs, String title) throws IOException {
+	private static long[] regularLoop(String s, int itrs, String title) throws IOException {
 		try (ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder())) {
 			out.writeUTF(s);
 			int len = s.length();
 			char[] dst = new char[len];
+			long[] rv = new long[2];
 			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null,
 					ByteOrder.nativeOrder())) {
-				System.out.println("Starting Regular " + title);
+				msg("Starting Regular %s\n", title);
 
 				long start = System.currentTimeMillis();
 				boolean ok = true;
@@ -119,8 +131,9 @@ public class TestSSE {
 				}
 
 				long elapsed = System.currentTimeMillis() - start;
+				rv[0] = elapsed;
 
-				System.out.printf("Regular[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
+				msg("Regular[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
 
 				if (!ok) {
 					System.out.println("Failed");
@@ -137,18 +150,23 @@ public class TestSSE {
 					in.reset();
 				}
 				elapsed = System.currentTimeMillis() - start;
+				rv[1] = elapsed;
+				msg("Regular(Inflate)[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
 
-				System.out.printf("Regular(Inflate)[%s]: Itrs: %d. Elapsed: %d\n", title, itrs, elapsed);
-
+				return rv;
 			}
 		}
 
 	}
 
+	private static void msg(String fmt, Object... args) {
+		// System.out.printf(fmt, args);
+	}
+
 	private static String loadTextSource(String str) throws IOException {
 		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(
-				new InputStreamReader(TestSSE.class.getClassLoader().getResourceAsStream("txt/" + str)))) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(
+				TestSSEDeserialization.class.getClassLoader().getResourceAsStream("txt/" + str)))) {
 			String l;
 			while ((l = br.readLine()) != null) {
 				sb.append(l);
