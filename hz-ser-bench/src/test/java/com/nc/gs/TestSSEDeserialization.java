d613f37635ebc4b02c0ab171ffdd19e769f544bc
cmuramoto <cmuramoto@nismo.nc.main>
2016-02-04 09:04:56 -0200

    Non ascii string in test

--- hz-ser-bench/src/test/java/com/nc/gs/TestSSEDeserialization.java
+++ hz-ser-bench/src/test/java/com/nc/gs/TestSSEDeserialization.java
@@ -1,8 +1,8 @@
 package com.nc.gs;
 
-import java.io.BufferedReader;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.ByteOrder;
@@ -14,57 +14,13 @@ import com.nc.io.ByteArrayObjectDataOutput;
 
 public class TestSSEDeserialization {
 
-	@SuppressWarnings("resource")
-	@Test
-	public void sample() throws IOException {
-		ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder());
-
-		out.writeUTF("One");
-		out.writeUTF("Two");
-
-		ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.toByteArray(), null, ByteOrder.nativeOrder());
-
-		assertEquals("One", in.readUTFJNI());
-		assertEquals("Two", in.readUTFJNI());
-	}
-
-	@Test
-	public void danteLoop() throws IOException {
-
-		loop("paradiseXXXIII.txt");
-	}
-
-	@Test
-	public void asciiLoop() throws IOException {
-		loop("pure_ascii.txt");
-	}
-
-	private static void loop(String resource) throws IOException {
-		msg("-----------------");
-		String s = loadTextSource(resource);
-
-		msg("Length %d\n", s.length());
-
-		regularLoop(s, 100, "Warm Up");
-		long[] rl = regularLoop(s, 100000, "Bench");
-
-		jniLoop(s, 100, "Warm Up");
-		long[] jl = jniLoop(s, 100000, "Bench");
-
-		msg("-----------------");
-
-		System.err.printf("\nString len: %d. Regular (elapsed): %d. JNI (elapsed): %d. Regular (elapsed/inflate-only): %d. JNI (elapsed/inflate-only): %d. \nRegular/JNI: %.2f.\nRegular/JNI (Inflate only): %.2f\n",s.length(), rl[0],jl[0],rl[1],jl[1], ((double) rl[0]) / jl[0],
-				((double) rl[1]) / jl[1]);
-	}
-
 	private static long[] jniLoop(String s, int itrs, String title) throws IOException {
 		try (ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder())) {
 			out.writeUTF(s);
 			int len = s.length();
 			char[] dst = new char[len];
 			long[] rv = new long[2];
-			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null,
-					ByteOrder.nativeOrder())) {
+			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null, ByteOrder.nativeOrder())) {
 				msg("Starting jni %s\n", title);
 
 				long start = System.currentTimeMillis();
@@ -108,14 +64,46 @@ public class TestSSEDeserialization {
 		}
 	}
 
+	private static String loadTextSource(String str) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(TestSSEDeserialization.class.getClassLoader().getResourceAsStream("txt/" + str)))) {
+			String l;
+			while ((l = br.readLine()) != null) {
+				sb.append(l);
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private static void loop(String resource) throws IOException {
+		msg("-----------------");
+		String s = loadTextSource(resource);
+
+		msg("Length %d\n", s.length());
+
+		regularLoop(s, 100, "Warm Up");
+		long[] rl = regularLoop(s, 100000, "Bench");
+
+		jniLoop(s, 100, "Warm Up");
+		long[] jl = jniLoop(s, 100000, "Bench");
+
+		msg("-----------------");
+
+		System.err.printf("\nString len: %d. Regular (elapsed): %d. JNI (elapsed): %d. Regular (elapsed/inflate-only): %d. JNI (elapsed/inflate-only): %d. \nRegular/JNI: %.2f.\nRegular/JNI (Inflate only): %.2f\n", s.length(), rl[0], jl[0], rl[1], jl[1], ((double) rl[0]) / jl[0], ((double) rl[1]) / jl[1]);
+	}
+
+	private static void msg(String fmt, Object... args) {
+		// System.out.printf(fmt, args);
+	}
+
 	private static long[] regularLoop(String s, int itrs, String title) throws IOException {
 		try (ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder())) {
 			out.writeUTF(s);
 			int len = s.length();
 			char[] dst = new char[len];
 			long[] rv = new long[2];
-			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null,
-					ByteOrder.nativeOrder())) {
+			try (ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.bytes(), null, ByteOrder.nativeOrder())) {
 				msg("Starting Regular %s\n", title);
 
 				long start = System.currentTimeMillis();
@@ -159,21 +147,30 @@ public class TestSSEDeserialization {
 
 	}
 
-	private static void msg(String fmt, Object... args) {
-		// System.out.printf(fmt, args);
+	@Test
+	public void asciiLoop() throws IOException {
+		loop("pure_ascii.txt");
 	}
 
-	private static String loadTextSource(String str) throws IOException {
-		StringBuilder sb = new StringBuilder();
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(
-				TestSSEDeserialization.class.getClassLoader().getResourceAsStream("txt/" + str)))) {
-			String l;
-			while ((l = br.readLine()) != null) {
-				sb.append(l);
-			}
-		}
+	@Test
+	public void danteLoop() throws IOException {
+		loop("paradiseXXXIII.txt");
+	}
 
-		return sb.toString();
+	@SuppressWarnings("resource")
+	@Test
+	public void sample() throws IOException {
+		ByteArrayObjectDataOutput out = new ByteArrayObjectDataOutput(256 * 1024, null, ByteOrder.nativeOrder());
+
+		String left = "OneÃº¹²£¢¬←ŋðßæ»©“”n¢ŧ®ħŋħŋµ“đ©»«ðßð/€²³£ŧ←ħ̉ øµ”“³©»";
+		out.writeUTF(left);
+		String right = "Two{¬¢£đŋ↓¬ħ←ŋŧđ®ð€ĸ→ħ←ŋŧ€³®đ¢→ĸøłþ[ĸ¬ħŋŧđ®ð€ß?ß®£ŧ¢ŋ←¬{→[ĸøþ´łøĸ→ħŋđ";
+		out.writeUTF(right);
+
+		ByteArrayObjectDataInput in = new ByteArrayObjectDataInput(out.toByteArray(), null, ByteOrder.nativeOrder());
+
+		assertEquals(left, in.readUTFJNI());
+		assertEquals(right, in.readUTFJNI());
 	}
 
 }
